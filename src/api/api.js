a77ebf22841b127855c2e8e26c1f205f65c1f5e9
Emmanuel-Dev-CEO <emmanueldev584@gmail.com>
2025-07-03 13:19:31 +0100

    commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const api = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://martafrik-api.onrender.com',
     withCredentials: true,
 })
 export default api
